537cabc1c4a9ee51840ffa0b378e7a9649cb64bf
Cody W. Opel <codyopel@gmail.com>
2019-03-05 17:42:41 -0500

    add default.nix

---
+++ default.nix
@@ -0,0 +1,14 @@
+with import <nixpkgs> { }; {
+  conveyorEnv = std.mkDerivation {
+    name = "conveyor-env";
+    buildInputs = [
+      stdenv
+      python3
+    ] ++ (with python3Packages; [
+      guessit
+      requests
+    ]) ++ (with goPackages [
+      rclone.bin
+    ]);
+  };
+}
